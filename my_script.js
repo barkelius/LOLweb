6ada23aaa81bcf5125e34464f32d9f6615494836
Axeba415 <barkelius@gmail.com>
2014-02-26 17:27:39 +0100

    Changed names and cleanup

--- my_script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready( function() {
-    $.getJSON("https://prod.api.pvp.net/api/lol/euw/v1.3/summoner/by-name/barkelius?api_key=818fcfce-3cb5-49eb-a38d-8ee45d6e3e76", function(data){
-        alert("hej: "+data);
-    });
-});
