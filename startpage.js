e081e834e0a90c5428f386946de30c72136b9cb8
Axeba415 <barkelius@gmail.com>
2014-06-01 15:45:08 +0200

    updated the website since riot changed its api, from v1.2 to 1.3 on several instances and fixing small errors.

--- startpage.js
+++ startpage.js
@@ -18,7 +18,7 @@ $(function getSummonerInfo() {
        document.getElementById('summonerIdToShowMe').innerHTML = id;
         //----------ranked stats(lite)---------------------------------------
            $.ajax({
-               url: 'https://prod.api.pvp.net/api/lol/' + window.summonerServer + '/v1.2/stats/by-summoner/' + id + '/ranked?season=SEASON4&api_key=818fcfce-3cb5-49eb-a38d-8ee45d6e3e76',
+               url: 'https://prod.api.pvp.net/api/lol/' + window.summonerServer + '/v1.3/stats/by-summoner/' + id + '/ranked?season=SEASON4&api_key=818fcfce-3cb5-49eb-a38d-8ee45d6e3e76',
                dataType: 'json',
                success: function(data) {
 
@@ -134,7 +134,7 @@ $(function getSummonerInfo() {
            });
 
            $.ajax({
-               url: 'https://prod.api.pvp.net/api/lol/' + window.summonerServer + '/v1.2/stats/by-summoner/' + id + '/ranked?season=SEASON4&api_key=818fcfce-3cb5-49eb-a38d-8ee45d6e3e76',
+               url: 'https://prod.api.pvp.net/api/lol/' + window.summonerServer + '/v1.3/stats/by-summoner/' + id + '/ranked?season=SEASON4&api_key=818fcfce-3cb5-49eb-a38d-8ee45d6e3e76',
                dataType: 'json',
                success: function(data) {
                    var x;  //mostPlayedChampionArrayPossition
@@ -148,9 +148,14 @@ $(function getSummonerInfo() {
                    var kda = (data.champions[x].stats.totalChampionKills/data.champions[x].stats.totalSessionsPlayed).toFixed(1) + "/" +
                        (data.champions[x].stats.totalDeathsPerSession/data.champions[x].stats.totalSessionsPlayed).toFixed(1) + "/" +
                        (data.champions[x].stats.totalAssists/data.champions[x].stats.totalSessionsPlayed).toFixed(1);
-
-
-                   document.getElementById('playerTopRightChampionName').innerHTML = data.champions[x].name;
+                    var championName;
+                   for(var i = 0; i < keys.length; ++i){
+                       if(keys[i][0] == data.champions[x].id){
+                           championName = keys[i][1];
+                           break;
+                       }
+                   }
+                   document.getElementById('playerTopRightChampionName').innerHTML = championName;
                    document.getElementById('playerTopRightChampionImage').innerHTML = "<img class='playerTopRightImage' src='championImages/" + data.champions[x].id + ".png'>";
                    document.getElementById('playerTopRightKDA').innerHTML = kda;
                    document.getElementById('playerTopRightChampionLosses').innerHTML = data.champions[x].stats.totalSessionsLost;
@@ -350,3 +355,124 @@ legend: {
 });
 });
     */
+
+var keys=[
+    [35, "Shaco"],
+    [36, "DrMundo"],
+    [33, "Rammus"],
+    [34, "Anivia"],
+    [39, "Irelia"],
+    [157, "Yasuo"],
+    [37, "Sona"],
+    [38, "Kassadin"],
+    [154, "Zac"],
+    [43, "Karma"],
+    [42, "Corki"],
+    [41, "Gangplank"],
+    [40, "Janna"],
+    [22, "Ashe"],
+    [23, "Tryndamere"],
+    [24, "Jax"],
+    [25, "Morgana"],
+    [26, "Zilean"],
+    [27, "Singed"],
+    [28, "Evelynn"],
+    [29, "Twitch"],
+    [161, "Velkoz"],
+    [3, "Galio"],
+    [2, "Olaf"],
+    [1, "Annie"],
+    [7, "Leblanc"],
+    [30, "Karthus"],
+    [6, "Urgot"],
+    [32, "Amumu"],
+    [5, "XinZhao"],
+    [4, "TwistedFate"],
+    [31, "Chogath"],
+    [9, "FiddleSticks"],
+    [8, "Vladimir"],
+    [19, "Warwick"],
+    [17, "Teemo"],
+    [18, "Tristana"],
+    [15, "Sivir"],
+    [16, "Soraka"],
+    [13, "Ryze"],
+    [14, "Sion"],
+    [11, "MasterYi"],
+    [12, "Alistar"],
+    [21, "MissFortune"],
+    [20, "Nunu"],
+    [107, "Rengar"],
+    [106, "Volibear"],
+    [105, "Fizz"],
+    [104, "Graves"],
+    [103, "Ahri"],
+    [99, "Lux"],
+    [102, "Shyvana"],
+    [101, "Xerath"],
+    [412, "Thresh"],
+    [98, "Shen"],
+    [222, "Jinx"],
+    [96, "KogMaw"],
+    [92, "Riven"],
+    [91, "Talon"],
+    [90, "Malzahar"],
+    [10, "Kayle"],
+    [89, "Leona"],
+    [79, "Gragas"],
+    [117, "Lulu"],
+    [114, "Fiora"],
+    [78, "Poppy"],
+    [115, "Ziggs"],
+    [77, "Udyr"],
+    [112, "Viktor"],
+    [113, "Sejuani"],
+    [110, "Varus"],
+    [111, "Nautilus"],
+    [119, "Draven"],
+    [82, "Mordekaiser"],
+    [83, "Yorick"],
+    [80, "Pantheon"],
+    [81, "Ezreal"],
+    [86, "Garen"],
+    [84, "Akali"],
+    [85, "Kennen"],
+    [67, "Vayne"],
+    [126, "Jayce"],
+    [69, "Cassiopeia"],
+    [127, "Lissandra"],
+    [68, "Rumble"],
+    [121, "Khazix"],
+    [122, "Darius"],
+    [120, "Hecarim"],
+    [72, "Skarner"],
+    [236, "Lucian"],
+    [74, "Heimerdinger"],
+    [238, "Zed"],
+    [75, "Nasus"],
+    [76, "Nidalee"],
+    [134, "Syndra"],
+    [59, "JarvanIV"],
+    [133, "Quinn"],
+    [58, "Renekton"],
+    [57, "Maokai"],
+    [56, "Nocturne"],
+    [55, "Katarina"],
+    [64, "LeeSin"],
+    [62, "MonkeyKing"],
+    [63, "Brand"],
+    [267, "Nami"],
+    [60, "Elise"],
+    [131, "Diana"],
+    [61, "Orianna"],
+    [266, "Aatrox"],
+    [143, "Zyra"],
+    [48, "Trundle"],
+    [45, "Veigar"],
+    [44, "Taric"],
+    [51, "Caitlyn"],
+    [53, "Blitzcrank"],
+    [54, "Malphite"],
+    [254, "Vi"],
+    [50, "Swain"]
+];
